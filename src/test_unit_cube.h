91690f7f9660d8fa1bd1845828b77233dfb99653
Mark Beiline <markbeiline@gmail.com>
2021-02-01 02:11:24 -0500

    Unnecessary line

--- src/test_unit_cube.h
+++ src/test_unit_cube.h
@@ -22,7 +22,6 @@ public:
 
         glm::mat4 modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -6.0f));
         modelMatrix = glm::rotate(modelMatrix, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
-        modelMatrix = glm::scale(modelMatrix, glm::vec3(1.01f, 1.01f, 1.01f));
 
         glm::mat4 viewMatrix = glm::mat4(1.0f);
 
