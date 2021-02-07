fe1b47f7a4d45f745b237a66b9bc658519ca3afe
Andrew H <andrew@plankdesign.com>
2021-02-07 12:16:21 -0500

    removed calls to view / proj matrix from module_axis

--- src/module_axis.h
+++ src/module_axis.h
@@ -45,17 +45,7 @@ public:
         Module::Render(world);
 
         glm::mat4 modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 1.0f, -6.0f));
-
-        glm::mat4 viewMatrix = glm::mat4(1.0f);
-
-        glm::mat4 projectionMatrix = glm::perspective(
-            glm::radians(45.0f), // field of view in degrees
-            world.windowAspectRatio,
-            0.01f, 100.0f); // near and far (near > 0)
-
         Resources::basicShader.setModelMatrix(modelMatrix);
-        Resources::basicShader.setViewMatrix(viewMatrix);
-        Resources::basicShader.setProjectionMatrix(projectionMatrix);
         Resources::basicShader.setColor(Resources::colorWhite);
 
         Resources::basicShader.activate();
