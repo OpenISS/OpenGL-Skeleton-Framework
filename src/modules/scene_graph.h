df98069bbde6542f6b0e0e28e3c77672772f4b45
Mark Beiline <markbeiline@gmail.com>
2021-02-16 16:32:58 -0500

    SceneGraph comments

--- src/modules/scene_graph.h
+++ src/modules/scene_graph.h
@@ -5,6 +5,18 @@
 
 class World;
 
+/**
+ * This is the hiearchical modelling system.
+ * 
+ * The hierarchy is composed of Node objects.
+ * A node can have any amount of children.
+ * A node's transform is propagated recursively onto all of its children.
+ * 
+ * Node subtypes can represent different scene objects (ex NodeModel).
+ * 
+ * @see Node
+ * @see NodeModel
+ */
 class SceneGraph : public Module
 {
 public:
@@ -13,9 +25,11 @@ public:
     void addChild(Node& child);
     void removeChild(Node& child);
 
+    /// The starting point of the hiearachy
     Node root;
 
 protected:
 
+    /// Recursively walks a node's children to resolving their transforms and render them
     void walkNode(World& world, Node& node, glm::mat4 stack);
 };
