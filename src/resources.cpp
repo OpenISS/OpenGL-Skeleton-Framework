f78a5746dbcc218f38dfe641e45b7464a4aa1c3d
Mark Beiline <markbeiline@gmail.com>
2021-03-15 22:10:32 -0400

    Added normals and UVs to unit cube mesh

--- src/resources.cpp
+++ src/resources.cpp
@@ -14,22 +14,43 @@ void Resources::initialize()
     // Unit cube
     unitCube.setBuffers(
     {
-        {glm::vec3(-0.5f, -0.5f, -0.5f), Resources::colorWhite}, // Bot left  (back)
-        {glm::vec3( 0.5f, -0.5f, -0.5f), Resources::colorWhite}, // Bot right (back)
-        {glm::vec3( 0.5f,  0.5f, -0.5f), Resources::colorWhite}, // Top right (back)
-        {glm::vec3(-0.5f,  0.5f, -0.5f), Resources::colorWhite}, // Top left  (back)
-        {glm::vec3(-0.5f, -0.5f,  0.5f), Resources::colorWhite}, // Bot left  (front)
-        {glm::vec3( 0.5f, -0.5f,  0.5f), Resources::colorWhite}, // Bot right (front)
-        {glm::vec3( 0.5f,  0.5f,  0.5f), Resources::colorWhite}, // Top right (front)
-        {glm::vec3(-0.5f,  0.5f,  0.5f), Resources::colorWhite}  // Top left  (front)
+        {glm::vec3(-0.5f, -0.5f, -0.5f), Resources::colorWhite, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(1.0f, 0.0f)}, // [Back] Bot right
+        {glm::vec3( 0.5f, -0.5f, -0.5f), Resources::colorWhite, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 0.0f)}, // [Back] Bot left
+        {glm::vec3( 0.5f,  0.5f, -0.5f), Resources::colorWhite, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 1.0f)}, // [Back] Top left
+        {glm::vec3(-0.5f,  0.5f, -0.5f), Resources::colorWhite, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(1.0f, 1.0f)}, // [Back] Top right
+
+        {glm::vec3(-0.5f, -0.5f,  0.5f), Resources::colorWhite, glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f)}, // [Front] Bot left
+        {glm::vec3( 0.5f, -0.5f,  0.5f), Resources::colorWhite, glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 0.0f)}, // [Front] Bot right
+        {glm::vec3( 0.5f,  0.5f,  0.5f), Resources::colorWhite, glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 1.0f)}, // [Front] Top right
+        {glm::vec3(-0.5f,  0.5f,  0.5f), Resources::colorWhite, glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f)}, // [Front] Top left
+
+        {glm::vec3(-0.5f,  0.5f,  0.5f), Resources::colorWhite, glm::vec3( 0.0f, 1.0f, 0.0f), glm::vec2(0.0f, 0.0f)}, // [Top] Bot left
+        {glm::vec3( 0.5f,  0.5f,  0.5f), Resources::colorWhite, glm::vec3( 0.0f, 1.0f, 0.0f), glm::vec2(1.0f, 0.0f)}, // [Top] Bot right
+        {glm::vec3( 0.5f,  0.5f, -0.5f), Resources::colorWhite, glm::vec3( 0.0f, 1.0f, 0.0f), glm::vec2(1.0f, 1.0f)}, // [Top] Top right
+        {glm::vec3(-0.5f,  0.5f, -0.5f), Resources::colorWhite, glm::vec3( 0.0f, 1.0f, 0.0f), glm::vec2(0.0f, 1.0f)}, // [Top] Top left
+
+        {glm::vec3(-0.5f, -0.5f, -0.5f), Resources::colorWhite, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 0.0f)}, // [Bot] Bot left
+        {glm::vec3( 0.5f, -0.5f, -0.5f), Resources::colorWhite, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(1.0f, 0.0f)}, // [Bot] Bot right
+        {glm::vec3( 0.5f, -0.5f,  0.5f), Resources::colorWhite, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(1.0f, 1.0f)}, // [Bot] Top right
+        {glm::vec3(-0.5f, -0.5f,  0.5f), Resources::colorWhite, glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 1.0f)}, // [Bot] Top left
+
+        {glm::vec3(-0.5f, -0.5f, -0.5f), Resources::colorWhite, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)}, // [Left] Bot left
+        {glm::vec3(-0.5f, -0.5f,  0.5f), Resources::colorWhite, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f)}, // [Left] Bot right
+        {glm::vec3(-0.5f,  0.5f,  0.5f), Resources::colorWhite, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 1.0f)}, // [Left] Top right
+        {glm::vec3(-0.5f,  0.5f, -0.5f), Resources::colorWhite, glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 1.0f)}, // [Left] Top left
+
+        {glm::vec3( 0.5f, -0.5f,  0.5f), Resources::colorWhite, glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)}, // [Left] Bot left
+        {glm::vec3( 0.5f, -0.5f, -0.5f), Resources::colorWhite, glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f)}, // [Left] Bot right
+        {glm::vec3( 0.5f,  0.5f, -0.5f), Resources::colorWhite, glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 1.0f)}, // [Left] Top right
+        {glm::vec3( 0.5f,  0.5f,  0.5f), Resources::colorWhite, glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 1.0f)}  // [Left] Top left
     },
     {
-        1, 0, 3, 1, 3, 2,
-        5, 1, 2, 5, 2, 6,
-        4, 5, 6, 4, 6, 7,
-        0, 4, 7, 0, 7, 3,
-        2, 3, 7, 2, 7, 6,
-        5, 4, 0, 5, 0, 1
+         1,  0,  3,  1,  3,  2, // Back
+         4,  5,  6,  4,  6,  7, // Front
+         8,  9, 10,  8, 10, 11, // Top
+        12, 13, 14, 12, 14, 15, // Bot
+        16, 17, 18, 16, 18, 19, // Left
+        20, 21, 22, 20, 22, 23  // Right
     });
 
     // Quad
